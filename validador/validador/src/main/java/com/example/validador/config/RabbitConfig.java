333e61b35128ccff5a84e801a311447290dc1cb6
Nicole <nicole.nogueira@idus.com.br>
2026-04-02 09:31:52 -0300

    feat: adicionando api validador no docker-compose.yml

--- validador/validador/src/main/java/com/example/validador/config/RabbitConfig.java
+++ validador/validador/src/main/java/com/example/validador/config/RabbitConfig.java
@@ -1,5 +1,8 @@
 package com.example.validador.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -7,9 +10,20 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
+
     @Bean
     public Queue queue() {
         return new Queue("cripto_queue", true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange("lamalis_exchange");
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("");
     }
 
     @Bean
